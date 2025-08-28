2d1ee917968189618b7b7063a749a70809c48b6d
Gia-Grace-Vansie <giagracevansie@gmail.com>
2025-08-28 11:43:11 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRtn1AlRtbgd8X_aTUrUYlWkwM7nxPvBs",
+  authDomain: "ggvca-goal-tracker.firebaseapp.com",
+  projectId: "ggvca-goal-tracker",
+  storageBucket: "ggvca-goal-tracker.firebasestorage.app",
+  messagingSenderId: "835155144288",
+  appId: "1:835155144288:web:e833c60128eab6d3c8dd05"
+};
+
+
+const app = initializeApp(firebaseConfig);
