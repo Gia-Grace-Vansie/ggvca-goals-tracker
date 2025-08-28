9593e29af50d3ed283df0db156709e80f7b790ed
Gia-Grace-Vansie <giagracevansie@gmail.com>
2025-08-28 10:12:10 +0800

    color for buttons

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#5f0d0dff',
     color: 'white',
     borderRadius: 8,
   }
