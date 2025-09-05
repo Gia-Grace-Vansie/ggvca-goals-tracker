cca285d485e5bb8a80c2b9594c291a266109d648
Gia-Grace-Vansie <giagracevansie@gmail.com>
2025-09-05 21:20:25 +0800

    Data inserted to the firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRtn1AlRtbgd8X_aTUrUYlWkwM7nxPvBs",
+  authDomain: "ggvca-goal-tracker.firebaseapp.com",
+  projectId: "ggvca-goal-tracker",
+  storageBucket: "ggvca-goal-tracker.firebasestorage.app",
+  messagingSenderId: "835155144288",
+  appId: "1:835155144288:web:e833c60128eab6d3c8dd05"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
